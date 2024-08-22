696bb0d61cc2eb3b78d449342ee1b80d1089dbf9
Yekazad Software Center <91857222+cigerxwinislam@users.noreply.github.com>
2024-08-22 20:50:51 +0300

    sexerenu

--- av/app/components/tegihistin.jsx
+++ av/app/components/tegihistin.jsx
@@ -10,10 +10,10 @@ import { WhatsAppWidget } from 'react-whatsapp-widget';
 export function Tegihistin() {
   return (<div>
     <Alert style={{margin:7}} >
-     <Button  href="tel:+905443404645"><FaPhone  /></Button> Telefon Et
+     <Button  href="tel:+905416206945"><FaPhone  /></Button> Telefon Et
 
 
-  <WhatsAppWidget message=" Whatsapptan Mesajınızı Yazın" inputPlaceHolder="Mesajınızı Yazın" replyTimeText="En kısa zamanda dönüş yapılacaktır" sendButtonText="Gönder" companyName="" phoneNumber="+905443404645" ></WhatsAppWidget>
+  <WhatsAppWidget message=" Whatsapptan Mesajınızı Yazın" inputPlaceHolder="Mesajınızı Yazın" replyTimeText="En kısa zamanda dönüş yapılacaktır" sendButtonText="Gönder" companyName="" phoneNumber="+905416206945" ></WhatsAppWidget>
 
   </Alert>
     </div>
