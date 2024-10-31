b151a27e43105646389e3276612a8d3476c1b6de
Yekazad Software Center <91857222+cigerxwinislam@users.noreply.github.com>
2024-10-31 12:25:46 +0300

    Update nav.tsx

--- av/app/components/nav.tsx
+++ av/app/components/nav.tsx
@@ -11,7 +11,7 @@ function BasicExample() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home">Avukat Burak Coşkun</Navbar.Brand>
+        <Navbar.Brand href="#home">Mimar Nurullah Demir</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
